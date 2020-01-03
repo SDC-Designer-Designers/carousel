1136c874197b8f1b96b9c5b3ce9ad526b513542d
Aria Keshmiri <ariakesh@yahoo.com>
2020-01-02 19:44:14 -0800

    async datGen

--- helpers/dataGen.js
+++ helpers/dataGen.js
@@ -3,12 +3,39 @@ const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 const randomNum = max => {
   return Math.floor(Math.random() * max);
   }
-  bar1.start(2500000, 0);
-for(var i = 1; i <  2500000 + 1; i++){
+//   bar1.start(2500000, 0);
+// for(var i = 1; i <  2500000 + 1; i++){
  
-  bar1.update(i)
-  let room = randomNum(5) + 1;
-  var str = `${i}	{https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))}}	${room}	${room * 2}	${randomNum(200)}	${randomNum(100)}	${randomNum(600)}	{${randomNum(1000)},${randomNum(1000)},${randomNum(1000)},${randomNum(1000)},${randomNum(1000)}} `
-  console.log(str)
+//   bar1.update(i)
+//   let room = randomNum(5) + 1;
+//   var str = `${i}	{https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))}}	${room}	${room * 2}	${randomNum(200)}	${randomNum(100)}	${randomNum(600)}	{${randomNum(1000)},${randomNum(1000)},${randomNum(1000)},${randomNum(1000)},${randomNum(1000)}} `
+//   console.log(str)
+// }
+// bar1.stop()
+
+const getStr = string => {
+  return string
 }
-bar1.stop()
+
+// const forLoop = async _ => {
+ 
+
+//   for (let i = 1; i < 2500000; i++) {
+
+//   }
+
+//   console.log('End')
+// }
+
+const forLoop = async _ => {
+  bar1.start(2500000, 0);
+  for(var i = 1; i <  10 + 1; i++){
+    bar1.update(i)
+    let room = randomNum(5) + 1;
+    var str = `${i}	{https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))},https://unsplash.it/500/500?image=${randomNum(800+randomNum(100))}}	${room}	${room * 2}	${randomNum(200)}	${randomNum(100)}	${randomNum(600)}	{${randomNum(1000)},${randomNum(1000)},${randomNum(1000)},${randomNum(1000)},${randomNum(1000)}} `
+    const string = await getStr(str)
+    console.log(string)
+  }
+  bar1.stop()
+}
+forLoop()
