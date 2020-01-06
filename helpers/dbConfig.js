534277b7e021a85bb272be685c938d64099fd972
Aria Keshmiri <ariakesh@yahoo.com>
2020-01-06 07:22:14 -0800

    generates 10M records seeds database succesfully on  ec2, updated README

--- helpers/dbConfig.js
+++ helpers/dbConfig.js
@@ -1,2 +1,3 @@
+// edit connectionString - postgressql://(add db_user here:add db_password here@localhost:5432/add db_name here)
 const connectionString = 'postgressql://ariakesh:shawdy123@localhost:5432/ariakesh'
 module.exports = connectionString
