0e4b301032760ae22ccb63f289b0dd01f8c2c712
Ubuntu <ubuntu@ip-172-31-84-149.ec2.internal>
2020-01-10 22:58:48 +0000

    connection string change ip

--- helpers/dbConfig.js
+++ helpers/dbConfig.js
@@ -1,4 +1,4 @@
 // edit connectionString - postgressql://(add db_user here:add db_password here@localhost:5432/add db_name here)
-const connectionString = 'postgressql://ariakesh:shawdy123@3.84.37.130/ariakesh'
+const connectionString = 'postgressql://ariakesh:shawdy123@18.233.153.135/ariakesh'
 console.log(connectionString)
 module.exports = connectionString
